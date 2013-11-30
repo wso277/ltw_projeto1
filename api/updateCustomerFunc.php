3d4121ea52e2043c4c3b84a6c77685fb82e66209
João Cardoso <jmmesquitacardoso@gmail.com>
2013-11-30 15:19:19 +0000

    update do cliente

--- api/updateCustomerFunc.php
+++ api/updateCustomerFunc.php
@@ -1,7 +1,7 @@
 <?php
 
-	function updateCustomer($customerID,$field,$newValue) {
-		if (isset($customerID) && isset($field) && isset($newValue)) {
+	function updateCustomer($customer,$update,$field) {
+		if (isset($customerID) && $customerID != "" isset($field) && $field != "" && isset($newValue) && $newValue != "") {
 
 			if ($customerID > 0) {
 				try {
@@ -11,8 +11,15 @@
 				}
 			}
 
-		$stmt = $db -> prepare("UPDATE Customer SET ". $field ."='". $newValue ."' WHERE CustomerID='". $customerID ."'");
-		$updateRes = $stmt -> execute();
+		if ($update != -1)
+		{
+			$stmt = $db -> prepare("UPDATE Customer SET ". $field ."='". $customer[$field] ."' WHERE CustomerID='". $customer['CustomerID'] ."'");
+			$updateRes = $stmt -> execute();
+		}
+		else
+		{
+			//insert
+		}
 		if (!$updateRes) {
 			return json_decode('{"error":{"code":601,"reason":"Error updating"}}', true);
 		}
